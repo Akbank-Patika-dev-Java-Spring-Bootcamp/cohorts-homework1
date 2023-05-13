c37c86320245a24a3679cb1840c5163c6331efb6
KaplanCoder <kemalselcukkaplan@gmail.com>
2023-05-14 01:13:30 +0300

    residence service created

--- src/service/ResidenceService.java
+++ src/service/ResidenceService.java
@@ -1,6 +1,35 @@
 package service;
 
+import domain.Residence;
+
+import java.util.List;
+
 public class ResidenceService {
+
+    public static double getTotalPrice(List<Residence> residences) {
+        double totalPrice = 0;
+        for (Residence residence: residences) {
+            totalPrice += residence.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public static int getTotalSquareMeter(List<Residence> residences) {
+        int totalSquareMeter = 0;
+        for (Residence residence: residences) {
+            totalSquareMeter += residence.getSquareMeter();
+        }
+        return totalSquareMeter;
+
+    }
+
+    public static List<Residence> getResidences(List<Residence> residences,int roomNumber,int saloonNumber) {
+       return  residences.stream().
+                filter(residence ->
+                        (residence.getRoomNumber() == roomNumber
+                                && residence.getSaloonNumber() == saloonNumber)).toList();
+
+    }
 
 
 }
