dff6014f4d55b8d3cb1a1fc760cc4c4867532e15
KaplanCoder <kemalselcukkaplan@gmail.com>
2023-05-14 02:38:09 +0300

    service methods added

--- src/service/ResidenceService.java
+++ src/service/ResidenceService.java
@@ -1,13 +1,47 @@
 package service;
 
+import domain.Home;
 import domain.Residence;
+import domain.SummerHouse;
+import domain.Villa;
 
 import java.util.List;
 
 public class ResidenceService {
 
-    public static double getTotalPrice(List<Residence> residences) {
-        double totalPrice = 0;
+    public static int getTotalPriceOfHomes(List<Home> homes) {
+        List<? extends Residence> upCastingHomes=homes;
+        return ResidenceService.getTotalPrice((List<Residence>) upCastingHomes);
+    }
+
+    public static int getTotalPricesOfVillas(List<Villa> villas) {
+        List<? extends Residence> upcastingVillas=villas;
+        return ResidenceService.getTotalPrice((List<Residence>) upcastingVillas);
+    }
+
+    public static int getTotalPricesOfSummerHouses(List<SummerHouse> summerHouses) {
+        List<? extends Residence> upcastingSummerHouses=summerHouses;
+        return ResidenceService.getTotalPrice((List<Residence>) upcastingSummerHouses);
+    }
+
+    public static int getTotalSquareMeterOfVillas(List<Villa> villas) {
+        List<? extends Residence> upcastingVillas=villas;
+        return ResidenceService.getTotalSquareMeter((List<Residence>) upcastingVillas);
+    }
+
+    public static int getTotalSquareMeterOfHomes(List<Home> homes) {
+        List<? extends Residence> upCastingHomes=homes;
+        return ResidenceService.getTotalSquareMeter((List<Residence>) upCastingHomes);
+    }
+
+    public static int getTotalSquareMeterOfSummerHouses(List<SummerHouse> summerHouses) {
+        List<? extends Residence> upcastingSummerHouses=summerHouses;
+        return ResidenceService.getTotalSquareMeter((List<Residence>) upcastingSummerHouses);
+    }
+
+
+    public static int getTotalPrice(List<Residence> residences) {
+        int totalPrice = 0;
         for (Residence residence: residences) {
             totalPrice += residence.getPrice();
         }
@@ -30,6 +64,7 @@ public class ResidenceService {
                                 && residence.getSaloonNumber() == saloonNumber)).toList();
 
     }
+
 
 
 }
